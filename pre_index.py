08b67b7669ad50da0c599d9d4a0e1c921b48efe2
Colin MacLeod <colinlmacleod1@gmail.com>
2017-11-15 14:48:52 -0500

    zfg

--- pre_index.py
+++ pre_index.py
@@ -81,5 +81,5 @@ handler({
 }, None)
 
 wav_player('new_rec2.wav')
-print (time.clock() - time_start)
+print ('Processing Time',time.clock() - time_start)
 #wav_player('you-really-need-to-grow-up.wav')
